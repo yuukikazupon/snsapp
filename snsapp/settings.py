3e25c359389dafc76016d304251b56b434d715c1
yuki <avfrr142@yahoo.co.jp>
2021-02-06 21:25:44 +0900

    first commit

--- snsapp/settings.py
+++ snsapp/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-DATABASES['default']= dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWADED_PROTO','https')
+# import dj_database_url
+# DATABASES['default']= dj_database_url.config()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWADED_PROTO','https')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- snsapp/wsgi.py
+++ snsapp/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsapp.settings.heroku')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
