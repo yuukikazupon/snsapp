6bfc9ac3058fa895e2117c80ceeedad5c351c542
yuki <avfrr142@yahoo.co.jp>
2021-02-14 22:01:17 +0900

    first commit

--- snsapp/asgi.py
+++ snsapp/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsappdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsapp.settings')
 
 application = get_asgi_application()
--- snsapp/wsgi.py
+++ snsapp/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsappdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snsapp.settings')
 
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
